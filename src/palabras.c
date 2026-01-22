81dfeee949a9f78193f0180caad0e84621334667
almumurillo <61943854+almumurillo@users.noreply.github.com>
2026-01-22 01:10:50 +0100

    Update palabras.c

--- src/palabras.c
+++ src/palabras.c
@@ -2,7 +2,6 @@
  * palabras.c
  *Vamos a empezar con estas palabras, podremos cambiarlas o añadir otras
  *  Implementación del módulo de gestión de palabras
- *  Cada línea tiene su comentario para que quede muy claro en la memoria:
  *  el array es static para que solo sea visible dentro del módulo, Palabras_Init se usa una vez al inicio de la partida (con una semilla tomada de un HAL_GetTick() o de una lectura ADC) y Palabras_GetRandom te da la palabra secreta para cada ronda.
  */
 
@@ -15,7 +14,8 @@ static const char* s_palabras[NUM_PALABRAS] =
     "CASAS", "PERRO", "GATOS", "LUCES", "RADAR",
     "PLAZA", "LIBRO", "PISTA", "CABLE", "FICHA",
     "NIVEL", "PARED", "ROBOT", "TECLA", "DATOS",
-    "JUEGO", "METAL", "RUIDO", "VALOR", "SIGMA"
+    "JUEGO", "METAL", "RUIDO", "VALOR", "SIGMA",
+    "MOVIL"
 };
 
 // Carga la semilla del generador pseudoaleatorio
@@ -30,4 +30,5 @@ const char* Palabras_GetRandom(void)
     uint32_t idx = rand() % NUM_PALABRAS;  // Índice entre 0 y NUM_PALABRAS-1
     return s_palabras[idx];                // Devuelve la palabra elegida
 }
+
 
